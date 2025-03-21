9bfbc25302de491eb15240deff9c85552027da62
AbdelrahmanSuliman <abdelrahmansuliman123@gmail.com>
2025-03-21 15:19:51 +0200

    Done media

--- src/main/java/dev/rubric/journalspring/repository/EntryRepository.java
+++ src/main/java/dev/rubric/journalspring/repository/EntryRepository.java
@@ -2,7 +2,6 @@ package dev.rubric.journalspring.repository;
 
 import dev.rubric.journalspring.models.Entry;
 import dev.rubric.journalspring.models.User;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
